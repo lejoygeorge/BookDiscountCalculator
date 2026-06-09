fef4457c44358d0ad7af995176568b4b012ba880
Lejoy George <lejoygeorge@Lejoys-MacBook-Air.local>
2026-06-09 10:49:05 +0530

    feat:Should return standard price with zero discount for a single item

--- src/main/java/com/bookorder/discountcalculator/service/PriceCalculatorService.java
+++ src/main/java/com/bookorder/discountcalculator/service/PriceCalculatorService.java
@@ -3,7 +3,14 @@ package com.bookorder.discountcalculator.service;
 import java.util.List;
 
 public class PriceCalculatorService {
+    public static final double BOOK_PRICE = 50.0;
     public double calculatePrice(List<Object> objects) {
-        return 0.0;
+        double finalPrice =0.0;
+        if(!objects.isEmpty()) {
+            if (objects.size() == 1) {
+                finalPrice = BOOK_PRICE;
+            }
+        }
+        return finalPrice;
     }
 }
--- src/test/java/com/bookorder/discountcalculator/service/PriceCalculatorServiceTest.java
+++ src/test/java/com/bookorder/discountcalculator/service/PriceCalculatorServiceTest.java
@@ -1,12 +1,16 @@
 package com.bookorder.discountcalculator.service;
 
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.when;
 
 class PriceCalculatorServiceTest {
 
@@ -22,5 +26,12 @@ class PriceCalculatorServiceTest {
     void testCalculatePrice_EmptyBasket() {
         double receipt = priceCalculatorService.calculatePrice(Collections.emptyList());
         assertEquals(0.0, receipt, 0.001);
+    }
+
+    @Test
+    @DisplayName("Should return standard price with zero discount for a single item")
+    void testCalculatePrice_SingleItem() {
+        double receipt = priceCalculatorService.calculatePrice(Arrays.asList(new Integer[]{1}));
+        assertEquals(50.0, receipt, 0.001);
     }
 }
